fe6d18d85d23fef21e1eb056fa622cfaa028ac63
seanErmilov <seanermilov321@gmail.com>
2024-06-13 15:14:13 +0300

    add utils

---
+++ js/utils.js
@@ -0,0 +1,105 @@
+'use strict'
+
+function createMat(rows, cols) {
+    const mat = []
+    for (var i = 0; i < rows; i++) {
+        const row = []
+        for (var j = 0; j < cols; j++) {
+            row.push('')
+        }
+        mat.push(row)
+    }
+    return mat
+}
+
+
+function getRandomInt(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+}
+
+function getRandomIntInclusive(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+}
+
+function findEmptyPos() {
+    //emptyPoss will be [{i:0,j:0},{i:0,j:1}]
+    var emptyPoss = []
+    for (var i = 0; i < gBoard.length; i++) {
+        for (var j = 0; j < gBoard.length; j++) {
+            var cell = gBoard[i][j]
+            if (!cell) {
+                // console.log('cell:', cell)
+                var pos = { i: i, j: j }
+                emptyPoss.push(pos)
+            }
+        }
+    }
+    var randIdx = getRandomInt(0, emptyPoss.length) // 0 , 1
+    var randPos = emptyPoss[randIdx] //{}
+    return randPos
+}
+
+function copyMat(mat) {
+    var newMat = []
+    for (var i = 0; i < mat.length; i++) {
+        newMat[i] = []
+        for (var j = 0; j < mat[0].length; j++) {
+            newMat[i][j] = mat[i][j]
+        }
+    }
+    return newMat
+}
+
+function countAround(cellI, cellJ) { // 7,0
+    var aroundCount = 0
+    for (var i = cellI - 1; i <= cellI + 1; i++) {
+        if (i < 0 || i >= gBoard.length) continue
+        for (var j = cellJ - 1; j <= cellJ + 1; j++) {
+            if (j < 0 || j >= gBoard[i].length) continue
+            if (i === cellI && j === cellJ) continue
+
+            if (gBoard[i][j] === LIFE) aroundCount++
+        }
+    }
+    return aroundCount
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function makeId(length = 6) {
+    var txt = ''
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+    for (var i = 0; i < length; i++) {
+        txt += possible.charAt(Math.floor(Math.random() * possible.length))
+    }
+
+    return txt
+}
+
+//timer - needs mins and secs class in index.html
+
+function setTime() {
+    var elMins = document.querySelector(`.mins`)
+    var elSecs = document.querySelector(`.secs`)
+    ++gTotalSeconds
+    elSecs.innerHTML = pad(gTotalSeconds % 60)
+    elMins.innerHTML = pad(parseInt(gTotalSeconds / 60))
+}
+
+function pad(val) {
+    var str = val + ''
+    if (str.length < 2) return "0" + str
+    else return str
+}
